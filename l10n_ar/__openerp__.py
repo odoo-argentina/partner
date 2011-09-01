38f461f899555948dd76b408f5033840c7fe4033
Bruno M. Villasanti <brunov@openunit.com.ar>
2011-08-31 23:09:24 -0300

    [REF] Provinces moved to res_country_states.xml [REF] PEP8 applied to __openerp__.py

--- l10n_ar/__openerp__.py
+++ l10n_ar/__openerp__.py
@@ -1,11 +1,33 @@
 {
-	"name" : "Localization for Argentina", # 
-	"version" : "1.0",
-	"author" : "Thymbra - Torre de Hanoi",
-	"category" : "Localisation/America",
-	"depends" : ["base","account"],
-	"init_xml" : [],
-	"demo_xml" : [],
-	"update_xml" : ["localizacion_argentina.xml"],
-	"active": False 
+    'name': 'Localization for Argentina',
+    'version': '1.0',
+    'author': 'Thymbra - Torre de Hanoi',
+    'category': 'Localisation/America',
+    'description': """
+        Modelo de Localización para Argentina
+        Incluye:
+            - Plan Contable
+            - Provincias
+            - Impuestos básicos
+            - Denominaciones de Partners y Contactos
+            - Moneda ( Peso Argentino )
+
+        Autor : Luis Falcon
+
+        www.thymbra.com
+
+        Encoding : UTF-8
+        Licencia : GPL
+    """,
+    'depends': [
+        'base',
+        'account',
+    ],
+    'init_xml': [],
+    'demo_xml': [],
+    'update_xml': [
+        'res_country_states.xml',
+        'localizacion_argentina.xml',
+    ],
+    'active': False,
 }
