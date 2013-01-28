dac4f99dffcc9da8d5f8e5c5d198cab7e3471573
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2013-01-28 15:36:17 -0300

    [FIX] Configurations minor errors

--- country.py
+++ country.py
@@ -20,8 +20,14 @@
 ##############################################################################
 from osv import fields, osv
 
-class Country(osv.osv):
+class country(osv.osv):
         _inherit = 'res.country'
+        _columns = {
+                'afip_code': fields.char('AFIP code', size=64, help='Codigo oficial del AFIP.'),
+        }
+
+class country_state(osv.osv):
+        _inherit = 'res.country.state'
         _columns = {
                 'afip_code': fields.char('AFIP code', size=64, help='Codigo oficial del AFIP.'),
         }
