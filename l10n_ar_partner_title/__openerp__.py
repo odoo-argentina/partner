03dbd8c4cafd61f91a6dc48f49f7eb071d6bfe9a
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2016-01-13 10:26:23 -0300

    [REF] Initial commit.

---
+++ l10n_ar_partner_title/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{'active': False,
+ 'author': 'OpenERP - Team de Localización Argentina',
+ 'category': 'Localization/Argentina',
+ 'demo_xml': [],
+ 'depends': ['base'],
+ 'description': 'Módulo de nombres y títulos para personas físicas y'
+ ' no físicas.\n\n\n\nIncluye:\n\n\n\n - '
+ 'Denominación de clientes, proveedores y contactos.\n\n\n\n',
+ 'init_xml': [],
+ 'installable': True,
+ 'license': 'AGPL-3',
+ 'name': 'Argentina - Nombre y títulos de personas físicas o no.',
+ 'data': ['data/res_partner_title.xml'],
+ 'version': '8.0.1.1',
+ 'website': 'https://launchpad.net/~openerp-l10n-ar-localization'}
